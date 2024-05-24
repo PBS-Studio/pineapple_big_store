832b468fe32133dcdd13773cc425ca14b180ab22
EvilEagleWhale <165990030+EvilEagleWhale@users.noreply.github.com>
2024-05-24 15:17:44 +0800

    just_go_down update

---
+++ just_go_down/Scenes/end_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	visible = false
+
+
+
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://just_go_down/Scenes/wrold.tscn")
