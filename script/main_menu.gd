5bd1ba00918c1eb3464824f4ca518cbc04fe2657
kaiyohugo <41114603+KAIYOHUGO@users.noreply.github.com>
2024-06-02 00:27:45 +0800

    feat: :sparkles: Add basic menu transition

--- script/main_menu.gd
+++ script/main_menu.gd
@@ -11,3 +11,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	background.scroll_offset -= Vector2(1, 1) * 40 * delta
+
+
+
+
+func _on_button_click():
+	var options = SceneManager.create_options(1.0, "squares")
+	var general_options = SceneManager.create_general_options(ProjectSettings["application/boot_splash/bg_color"])
+	SceneManager.change_scene("pineapple_field", options, options, general_options)
