6fe4ffe3f70eacf8c1ab8649c77460258410df34
kaiyohugo <41114603+KAIYOHUGO@users.noreply.github.com>
2024-06-06 00:34:32 +0800

    fix: :bug: Fix bar story not play

--- script/bar.gd
+++ script/bar.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
+var limit = [2, - 42, 1154, 678]
+
 @onready var spin = $Spin
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if RpgManager.is_first_enter():
-		Dialogic.start("end")
+	Dialogic.start("end")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,7 +22,7 @@ func _on_exit_body_entered(body):
 	mihon.set_process(false)
 	var camera = mihon.get_node("Camera2D")
 	var tween = create_tween()
-	tween.tween_property(camera, "global_position", spin.global_position, 3.0)
-	tween.parallel().tween_property(camera, "zoom", 6 * Vector2.ONE, 5.0)
-	
+	tween.tween_property(camera, "global_position", spin.global_position, 2.0)
+	tween.parallel().tween_property(camera, "zoom", 6 * Vector2.ONE, 6.0)
+
 	# TODO
