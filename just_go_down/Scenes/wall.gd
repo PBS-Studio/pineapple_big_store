e1a89735a45eb6dcab224a20579ec28573323b1b
EvilEagleWhale <165990030+EvilEagleWhale@users.noreply.github.com>
2024-06-16 21:26:29 +0800

    法克油

--- just_go_down/Scenes/wall.gd
+++ just_go_down/Scenes/wall.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 #var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var score = 0
-
+var iscomplte = false
 func _ready():
 	Bgm.stream_paused = true
 	$justgodownbgm.stream_paused = false
@@ -12,6 +12,7 @@ func _ready():
 	update_score()
 	
 func completegame():
+	iscomplte = true
 	Bgm.stream_paused = false
 	RpgManager.change_scene_and_tp("big_road","ExitCollegeRoad")
 
@@ -19,7 +20,7 @@ func update_score():
 	$Label.text = str(floori(score))
 
 func _physics_process(delta):
-	if score >= 100.0:
+	if score >= 100.0 and !iscomplte:
 		completegame()
 	if Input.is_action_just_pressed("p"):
 		completegame()
